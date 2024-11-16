93ca199ff10019447d7c978d217e7f4019ad710b
javohir <ajavohir969@gmail.com>
2024-11-17 01:51:25 +0500

    localstorage

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,17 +1,22 @@
-export const initialState = {
+export const initialState = JSON.parse(localStorage.getItem("storage")) || {
     count: 1,
     wishlist: [],
     countwish: 0,
     card: []
 }
 export const reducer = (state, action) => {
+    let result = state
     switch (action.type) {
         case "ADD_WIShLIST":
             let index = state.wishlist.findIndex(pro => pro.id === action.payload.id)
             if (index < 0) {
-                return { ...state, wishlist: [...state.wishlist, action.payload] }
+                result = { ...state, wishlist: [...state.wishlist, action.payload] }
+                localStorage.getItem("storage", JSON.stringify(result))
+                return result
             } else {
-                return { ...state, wishlist: state.wishlist.filter(pro => pro.id !== action.payload.id) }
+                result = { ...state, wishlist: state.wishlist.filter(pro => pro.id !== action.payload.id) }
+                localStorage.getItem("storage", JSON.stringify(result))
+                return result
             }
         case "INC":
             return { ...state, count: state.count + 1 }
